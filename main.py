096f6e4b8743ccc8216b0abf070a53fc74962382
DavidCaaveral <dcanaveral4.5+1@gmail.com>
2026-05-21 19:36:16 -0500

    funciones de compuertas logicas realizadas

--- main.py
+++ main.py
@@ -1,15 +1,53 @@
 # 1. Importaciones de librerías
+import os
+from colorama import init, Fore, Back, Style
 
 
 
 # 2. Definición de constantes globales
-
+statementObj = { # Inicialización de variables de objeto
+    'A': -1,
+    'B': -1,
+    'C': -1,
+    'D': -1
+}
 
 
 # 3. Definición de funciones
 
 def andLogicGate(a,b) -> int:
     return a*b
+
+def orLogicGate(a,b) -> int:
+    return (a+b) - 1 if (a+b) > 1 else a+b
+
+def notLogicGate(a) -> int:
+    return 1 - a
+
+def xorLogicGate(a,b) -> int:
+    return a ^ b
+
+def ifLogicGate(a) -> int:
+    return  (a) - 1 if a > 1 else a 
+
+def nAndLogicGate(a,b) -> int:
+    localAnd = andLogicGate(a,b)
+    return notLogicGate(localAnd)
+
+def nOrLogicGate(a,b) -> int:
+    localOr = orLogicGate(a,b)
+    return notLogicGate(localOr)
+
+def xnorLogicGate(a,b) -> int:
+    localXor = xorLogicGate(a,b)
+    return notLogicGate(localXor)
+
+def clearTerm() -> None:
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
+
 
 # 4. Definición de clases (opcional)
 
@@ -19,7 +57,6 @@ def andLogicGate(a,b) -> int:
 # 5. Bloque principal de ejecución
 def main():
     pass
-
 
 # 6. Punto de entrada
 if __name__ == "__main__":
