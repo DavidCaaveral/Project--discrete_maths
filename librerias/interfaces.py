ee1370175dafecfc24d3a01e1e2b077040c5ee7e
xpenkox-beep <xpenkox@gmail.com>
2026-05-26 17:27:34 -0500

    Decoracion de Menus y Submenus con Colorama

--- librerias/interfaces.py
+++ librerias/interfaces.py
@@ -3,6 +3,7 @@ from colorama import init, Fore, Back, Style
 import inquirer
 from main import statementObj
 from main import clearTerm
+import os
 
 init()
 
@@ -61,6 +62,7 @@ def mainMenu() -> dict:
             message="Seleccione una opción",
             choices=[
                 ("Ingresar variables booleanas (A, B, C, D)", "var-bool"),
+                ("Mostrar valor actual de las variables", "actua-var-bool"),
                 ("Ir al Submenú 1 (Lector de Compuertas)", "submenu1"),
                 ("Ir al Submenú 2 (Circuitos Lógicos)", "submenu2"),
                 ("Limpiar pantalla", "LIMPIAR"),
@@ -75,21 +77,48 @@ def subMenu1(objVar) -> dict:
     print(" "*30, f"> Variables booleanas: A:{statementObj['A']}, B:{statementObj['B']}, C:{statementObj['C']}, D:{statementObj['D']}")
     optionsSubMenu = [
         inquirer.List(
-            "submenu",
-            message="Seleccione una opcion",
+            "submenu", 
+            message= (f"\n{Fore.YELLOW}{"►"*12}{Style.RESET_ALL}{Fore.MAGENTA}  COMPUERTAS LÓGICAS {Style.RESET_ALL}{Fore.YELLOW}{"◄"*12}{Style.RESET_ALL}\n"),
+
             choices=[
-            ("compuerta AND", "AND"),
-            ("compuerta OR", "OR"),
-            ("compuerta NOT", "NOT"),
-            ("compuerta XOR", "XOR"),
-            ("compuerta NAND", "NAND"),
-            ("compuerta NOR", "NOR"),
-            ("compuerta XNOR", "XNOR"),
-            ("compuerta IF", "IF"),
-            ("Volver al menú principal", "VOLVER"),
-            ("limpiar terminal", "LIMPIAR")
+                (f"{Fore.LIGHTYELLOW_EX}►{Style.RESET_ALL}COMPUERTA {Fore.MAGENTA}AND{Style.RESET_ALL}", "AND"),
+                (f"{Fore.LIGHTYELLOW_EX}► {Style.RESET_ALL}COMPUERTA {Fore.MAGENTA}OR{Style.RESET_ALL}", "OR"),
+                (f"{Fore.LIGHTYELLOW_EX}► {Style.RESET_ALL}COMPUERTA {Fore.MAGENTA}NOT{Style.RESET_ALL}", "NOT"),
+                (f"{Fore.LIGHTYELLOW_EX}► {Style.RESET_ALL}COMPUERTA {Fore.MAGENTA}XOR{Style.RESET_ALL}", "XOR"),
+                (f"{Fore.LIGHTYELLOW_EX}► {Style.RESET_ALL}COMPUERTA {Fore.MAGENTA}NAND{Style.RESET_ALL}", "NAND"),
+                (f"{Fore.LIGHTYELLOW_EX}► {Style.RESET_ALL}COMPUERTA {Fore.MAGENTA}NOR{Style.RESET_ALL}", "NOR"),
+                (f"{Fore.LIGHTYELLOW_EX}► {Style.RESET_ALL}COMPUERTA {Fore.MAGENTA}XNOR{Style.RESET_ALL}", "XNOR"),
+                (f"{Fore.LIGHTYELLOW_EX}► {Style.RESET_ALL}COMPUERTA {Fore.MAGENTA}IF{Style.RESET_ALL}", "IF"),
+                (f"{Fore.RED}► VOLVER AL MENÚ PRINCIPAL{Style.RESET_ALL}", "SALIR"),
+                (f"{Fore.GREEN}► LIMPIAR TERMINAL{Style.RESET_ALL}", "LIMPIAR")
                 ]
             )
     ]
     return inquirer.prompt(optionsSubMenu) 
     
+subMenu1(statementObj)
+
+optionsSubMenu2 = [ 
+     inquirer.List(
+         "submenu2",
+         message=(f"\n{Fore.YELLOW}{"►"*12}{Style.RESET_ALL}{Fore.MAGENTA} SELECCIONE LA GRÁFICA QUE DESEA VISUALIZAR {Style.RESET_ALL}{Fore.YELLOW}{"◄"*12}{Style.RESET_ALL}"),
+         choices=[
+             (f"{Fore.LIGHTYELLOW_EX}►{Style.RESET_ALL}{Fore.MAGENTA}GRÁFICA #1{Style.RESET_ALL}", "LITERAL1"),
+             (f"{Fore.LIGHTYELLOW_EX}►{Style.RESET_ALL}{Fore.MAGENTA}GRÁFICA #2{Style.RESET_ALL}", "LITERAL2"),
+             (f"{Fore.LIGHTYELLOW_EX}►{Style.RESET_ALL}{Fore.MAGENTA}GRÁFICA #3{Style.RESET_ALL}", "LITERAL3"),
+             (f"{Fore.RED}► VOLVER ATRAS{Style.RESET_ALL}", "SALIR")
+         ]
+     )
+ ]
+
+answersubMenu2 = inquirer.prompt(optionsSubMenu2)
+print(f"\n{Fore.YELLOW} {"►"*8} {Fore.MAGENTA}GRAFICAS{Style.RESET_ALL} {Fore.YELLOW} {"◄"*8} {Style.RESET_ALL}\n")
+print(answersubMenu2["submenu2"])
+print(f"="*66+'\n')
+
+if answersubMenu2["submenu2"] == "►GRÁFICA #1":
+        print ("\n----------------------------GRÁFICA #1----------------------------\n")
+elif answersubMenu2["submenu2"] == "►GRÁFICA #2":
+        print ("\n----------------------------GRÁFICA #2----------------------------\n")
+elif answersubMenu2["submenu2"] == "►GRÁFICA #3":
+        print ("\n----------------------------GRÁFICA #3----------------------------\n")
