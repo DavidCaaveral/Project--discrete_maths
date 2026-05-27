c2a73120b47c11b617712518f40bc288a8513a99
DavidCaaveral <dcanaveral4.5+1@gmail.com>
2026-05-27 16:50:28 -0500

    circuitos añadidos

--- librerias/circuitos.py
+++ librerias/circuitos.py
@@ -18,4 +18,65 @@ def secondCircuitDiagram(a,b,c,keyA,keyB,keyC) -> str:
 {keyC}: {c} -----------------|______/
 """
 
-print(secondCircuitDiagram(1,0,1,'A','B','C'))
+def firstCircuitDiagram(a,b,c,d,keyA,keyB,keyC,keyD) -> str:
+    firstLocalAnd = andLogicGate(a,b)
+    unicLocalNand = nAndLogicGate(a,c)
+    notD = notLogicGate(d)
+    firstLocalOr = orLogicGate(firstLocalAnd,b)
+    unicLocalIf = ifLogicGate(firstLocalOr)
+    unicLocalXor = xorLogicGate(unicLocalIf,unicLocalNand)
+    unicLocalNor = nOrLogicGate(unicLocalXor,notD)
+    
+    return f"""
+                  ______
+{keyA}: {a} ----+-------|      \\     
+         |       | AND   )-----------|      _____              |\\
+{keyB}: {b} -+--)-------|______/            |     \\     \\             |  \\
+      |  |                           -------) OR  )------------|IF >-----------|
+      |  |                           |     /_____/             |  /            |      _____
+      ---)---------------------------|                         |/              |    \\\\     \\
+         |                                                                     ------)) XOR )-----------|
+         |        ______                                                       |    //_____/            |
+         --------|      \\                                                      |                        |      _____       
+                 | NAND  )o----------------------------------------------------|                        |     \\     \\ 
+{keyC}: {c} ------------|______/                                                                               -------) NOR )o----------  F: {unicLocalNor}
+                                                                                                        |     /_____/     
+                                                                                                        |
+                 |\\                                                                                     |
+                 |  \\                                                                                   |
+{keyD}: {d} ------------|NOT>o---------------------------------------------------------------------------------|
+                 |  / 
+                 |/   
+      
+
+"""
+
+def thirdCircuitDiagram(a,b,c,d,keyA,keyB,keyC,keyD) -> str:
+    notA = notLogicGate(a)
+    firstLocalXnor = xNorLogicGate(b,c)
+    unicLocalAnd = andLogicGate(b,d)
+    unicLocalOr = orLogicGate(notA,firstLocalXnor)
+    secondLocalXnor = xNorLogicGate(unicLocalOr,unicLocalAnd)
+    
+    return f"""
+                             
+               |\\            
+               |  \\          
+{keyA}: {a} ----------|NOT>o-------------
+               |  /              |      _____
+               |/                |     \\     \\
+                                 -------) OR  )--------
+                   _____         |     /_____/        |
+{keyB}: {b} ------+-----\\\\     \\        |                    |        _____   
+           |      ))XNOR )o-------                    |      \\\\     \\
+{keyC}: {c} ------)-----//_____/                             --------))XNOR )------- R: {secondLocalXnor}
+           |                                          |      //_____/    
+           |       ______                             |
+           -------|      \\                            |
+                  | AND   )----------------------------
+{keyD}: {d} -------------|______/
+
+
+"""
+
+print(thirdCircuitDiagram(1,0,1,0,'A','B','C','D'))
